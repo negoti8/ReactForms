71eb1ab9bf54b988bf773bdeef1632079e59c19e
briankim <brianhkim13@gmail.com>
2023-04-05 11:01:55 -0500

    multiple inputs unfinished

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 const Form = () => {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState('');
 
   const handleChange = (event) => {
     setUsername(event.target.value)
@@ -12,7 +13,9 @@ const Form = () => {
     event.preventDefault();
     const username = event.target.username.value;
     console.log(username);
+    console.log(password)
     setUsername('')
+    setPassword('')
   };
 
   return (
@@ -21,7 +24,10 @@ const Form = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
         <input type="text" name="username" value={username} onChange={handleChange} />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' name='password' value={password} onChange={(event) => setPassword(event.target.value)} />
         <button type="submit">Submit</button>
+        
       </form>
     </div>
   );
