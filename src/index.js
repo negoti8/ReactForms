b3031f793e5b6253c54cf0b27cf472cb27940d87
Mark Howell <markedhowell@gmail.com>
2023-04-05 09:31:04 -0600

    commiting controlled blank

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Form = () => {
+  const [username, setUsername] = useState("");
   const handleSubmit = (event) => {
-    event.preventDefault()
-    const username = event.target.username.value
-    console.log(username)
-  }
-  
+    event.preventDefault();
+    const username = event.target.username.value;
+    console.log(username);
+  };
+
   return (
-    <div id='container'>
-      <div id='navbar'>
-        Form.js
-      </div>
+    <div id="container">
+      <div id="navbar">Form.js</div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor='username'>Username:</label>
-        <input type='text' name='username' />
-        <button type='submit'>Submit</button>
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" value={username} />
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-
-
-ReactDOM.render(
-  <Form />,
-  document.getElementById('app')
-)
+ReactDOM.render(<Form />, document.getElementById("app"));
